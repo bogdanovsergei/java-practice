bf650d1502a9ea7a76b8668ea17b287a95c7104c
apple <apple@apples-MacBook-Air.local>
2019-05-15 01:14:45 -0400

    modified test class

--- src/day44_constractor02/WhatsAppMessages.java
+++ src/day44_constractor02/WhatsAppMessages.java
@@ -4,6 +4,8 @@ public class WhatsAppMessages {
 	public static void main(String[] args) {
 		WhatsApp wapp = new WhatsApp("703-333-4321", "Good for you!");
 		WhatsApp wapp2 = new WhatsApp("347-689-1200");
+		System.out.println("test");
 	}
 
+	
 }
